0861839e67129f28c124358307317f220d25dcae
Alex <weiru@hotmail.ca>
2024-04-13 19:38:47 -0400

    Update links

--- components/navbar/nav-links.tsx
+++ components/navbar/nav-links.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 const links = [
   { title: "Home", href: "/" },
+  { title: "About", href: "/about" },
   { title: "Gallery", href: "/gallery" },
   { title: "Projects", href: "/projects" },
   { title: "Recipes", href: "/recipes" },
@@ -9,17 +10,17 @@ const links = [
 
 const NavLinks = () => {
   return (
-    <div className="flex flex-wrap w-1/2 justify-end pr-[5.5rem]">
-      {links.map((link) => (
-        <Link
-          href={link.href}
-          key={link.title}
-          className="px-2 uppercase hover:opacity-60"
-        >
-          {link.title}
-        </Link>
-      ))}
-    </div>
+    <nav className="flex flex-wrap w-1/2 justify-end pr-[5.5rem]">
+      <ul className="list-none flex">
+        {links.map((link) => (
+          <li key={link.title}>
+            <Link href={link.href} className="px-2 uppercase hover:opacity-60">
+              {link.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
