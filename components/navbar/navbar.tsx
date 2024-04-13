d6c4d8bb15b83f05031de366b0bc68eddc00b0b5
Alex <weiru@hotmail.ca>
2024-04-13 19:38:54 -0400

    Update navbar style

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,7 +2,7 @@ import NavLinks from "./nav-links";
 
 const Navbar = () => {
   return (
-    <div className="flex w-full justify-between pt-3">
+    <div className="flex w-full justify-between pt-3 z-50 fixed top-0 text-white">
       <p className="w-1/2 uppercase px-24">Alex Ru</p>
       <NavLinks />
     </div>
