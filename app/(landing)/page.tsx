5c3e5723bec22f5f51894382226203743432ed37
Alex <weiru@hotmail.ca>
2024-04-13 19:37:06 -0400

    Add transitioning images to page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,20 @@
-const HomePage = () => {
-  return <div>homepage</div>;
+import HomeDisplay from "@/app/(landing)/components/home-display";
+import prismadb from "@/lib/prismadb";
+import BelowDisplay from "./components/below-display";
+
+const HomePage = async () => {
+  const images = await prismadb.images.findMany({ where: { width: 6000 } });
+
+  return (
+    <main>
+      <div className="flex flex-col">
+        <HomeDisplay images={images} />
+        <BelowDisplay />
+      </div>
+    </main>
+  );
 };
 
 export default HomePage;
+
+// className="min-h-[500px] mt-10 pb-[30px] small:mt-0 xsmall:mt-0 px-24"
