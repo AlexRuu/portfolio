358b771fc1357712cfa8cbd87934e24780c4f8f4
Alex <weiru@hotmail.ca>
2024-04-20 00:52:09 -0400

    Add animation to image transition

--- components/gallery/gallery-item.tsx
+++ components/gallery/gallery-item.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { Images } from "@/types";
 import { AspectRatio } from "../ui/aspect-ratio";
 import Image from "next/image";
 import { Dialog, DialogTrigger, DialogContent } from "../ui/dialog";
 import { useEffect, useState } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Images } from "@prisma/client";
+import { cn } from "@/lib/utils";
 
 interface GalleryItemProps {
   image: Images;
@@ -62,13 +63,19 @@ const GalleryItem: React.FC<GalleryItemProps> = ({ image, images }) => {
           </button>
         </div>
         <AspectRatio ratio={image.width / image.height}>
-          <Image
-            src={images[index].url}
-            alt="Gallery Image"
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="object-contain max-h-[calc(100vh-64px)] w-full"
-          />
+          {images.map((image, imageIndex) => (
+            <Image
+              key={image.id}
+              src={image.url}
+              alt="Gallery Image"
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className={cn(
+                "object-contain max-h-[calc(100vh-64px)] w-full cursor-default select-none absolute top-0 left-0 block transition-opacity duration-500 ease-in",
+                index === imageIndex ? "opacity-100 z-1" : "opacity-0 z-0"
+              )}
+            />
+          ))}
         </AspectRatio>
         <div className="relative self-center">
           <button>
