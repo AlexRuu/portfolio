6b7f98f1b097f43cf1938619639dd8a62b3c024a
Alex <weiru@hotmail.ca>
2024-04-20 00:50:43 -0400

    Add fade out animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,13 +69,18 @@ const config = {
         },
         "fade-in": {
           "0%": { opacity: "0" },
-          "100%": { opacity: "100", display: "block" },
+          "100%": { opacity: "100" },
+        },
+        "fade-out": {
+          "0%": { opacity: "100" },
+          "100%": { opacity: "0" },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "fade-in": "fade-in 0.7s ease-in",
+        "fade-out": "fade-out 0.3s ease-out",
       },
     },
   },
