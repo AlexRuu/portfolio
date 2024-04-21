cf11cca69a8b9722dbc656e6fb525cb73691f458
Alex <weiru@hotmail.ca>
2024-04-21 00:14:28 -0400

    Skeleton of about page

---
+++ app/about/page.tsx
@@ -0,0 +1,34 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import Image from "next/image";
+
+const AboutPage = () => {
+  return (
+    <main className="min-h-[500px] mt-10 pb-[30px] small:mt-0 xsmall:mt-0 px-24">
+      <div className="w-full ">
+        <h1 className="text-center">About</h1>
+        <div className="">
+          <AspectRatio ratio={6 / 4}>
+            <Image
+              src={
+                "https://res.cloudinary.com/dxnvintom/image/upload/v1713649339/me_ycb5uh.jpg"
+              }
+              alt="Profile"
+              height={400}
+              width={600}
+              className="object-contain"
+            />
+          </AspectRatio>
+        </div>
+        <div>
+          <p>
+            Hello, I&apos;m Alex! Welcome to my little corner on the internet
+            where I can share a bit about myself and my creations. I&apos;m a
+            developer and photographer with a background in chemistry.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AboutPage;
