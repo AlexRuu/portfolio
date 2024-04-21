355efd2de8e65242559e78d270644de6f66ce35b
Alex <weiru@hotmail.ca>
2024-04-21 00:15:00 -0400

    Change nav style

--- components/navbar/nav-links.tsx
+++ components/navbar/nav-links.tsx
@@ -1,25 +1,65 @@
-import Link from "next/link";
+"use client";
 
-const links = [
-  { title: "Home", href: "/" },
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
+
+const sublinks = [
   { title: "About", href: "/about" },
   { title: "Gallery", href: "/gallery" },
   { title: "Projects", href: "/projects" },
   { title: "Recipes", href: "/recipes" },
 ];
 
+const links = [
+  { title: "Home", href: "/" },
+  { title: "Projects", href: "/projects", submenu: sublinks },
+];
+
 const NavLinks = () => {
   return (
     <nav className="flex flex-wrap w-1/2 justify-end pr-[5.5rem]">
-      <ul className="list-none flex">
-        {links.map((link) => (
-          <li key={link.title}>
-            <Link href={link.href} className="px-2 uppercase hover:opacity-60">
-              {link.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NavigationMenu>
+        <NavigationMenuList>
+          {links.map((link) => (
+            <NavigationMenuItem key={link.title}>
+              {link.submenu ? (
+                <>
+                  <NavigationMenuTrigger>{link.title}</NavigationMenuTrigger>
+                  <NavigationMenuContent className="text-black">
+                    <ul>
+                      {link.submenu.map((sublink) => (
+                        <li key={sublink.title}>
+                          <Link href={sublink.href} legacyBehavior passHref>
+                            <NavigationMenuLink
+                              className={navigationMenuTriggerStyle()}
+                            >
+                              {sublink.title}
+                            </NavigationMenuLink>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </>
+              ) : (
+                <Link href={link.href} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    {link.title}
+                  </NavigationMenuLink>
+                </Link>
+              )}
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
     </nav>
   );
 };
