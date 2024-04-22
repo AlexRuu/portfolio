c6ce0e236185dd983f210acd1dd25fa9dcd0a52e
Alex <weiru@hotmail.ca>
2024-04-21 23:35:34 -0400

    Add mobile navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -4,24 +4,35 @@ import { cn } from "@/lib/utils";
 import NavLinks from "./nav-links";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import MobileNavbar from "./mobile-nav";
 
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <div
-      className={cn(
-        "hidden md:flex w-full justify-between pt-3 z-[100] absolute text-lg",
-        pathname == "/" && "text-white"
-      )}
-    >
-      <Link
-        href={"/"}
-        className="h-10 w-max items-center justify-center rounded-md px-16 py-2"
+    <div>
+      <div
+        className={cn(
+          "hidden sm:flex w-full justify-between pt-3 z-[100] absolute",
+          pathname == "/" && "text-white"
+        )}
       >
-        Alex Ru
-      </Link>
-      <NavLinks />
+        <Link
+          href={"/"}
+          className="h-10 w-max items-center justify-center rounded-md px-4 py-2 hover:opacity-50"
+        >
+          Alex Ru
+        </Link>
+        <NavLinks />
+      </div>
+      <div
+        className={cn(
+          "sm:hidden flex w-full justify-between pt-3 z-[100] absolute",
+          pathname == "/" && "text-white"
+        )}
+      >
+        <MobileNavbar />
+      </div>
     </div>
   );
 };
