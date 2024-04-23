d9ddc4d66912ea250fa3a022dcd7a094c403429b
Alex <weiru@hotmail.ca>
2024-04-23 00:11:08 -0400

    Create page

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/ui/header";
+
+const ProjectsPage = () => {
+  return (
+    <div>
+      <Header title="Projects" />
+    </div>
+  );
+};
+
+export default ProjectsPage;
