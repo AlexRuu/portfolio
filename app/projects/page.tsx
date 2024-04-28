1d7d8f839d13eee0d65f7d12d9def43bcab92d44
Alex <weiru@hotmail.ca>
2024-04-28 00:03:35 -0400

    Create projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,9 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Header from "@/components/ui/header";
+import prismadb from "@/lib/prismadb";
 import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 export const generateMetadata = (): Metadata => {
   return {
@@ -8,11 +12,47 @@ export const generateMetadata = (): Metadata => {
   };
 };
 
-const ProjectsPage = () => {
+const ProjectsPage = async () => {
+  const projects = await prismadb.project.findMany({
+    include: { projectImage: true },
+  });
+
   return (
-    <div>
-      <Header title="Projects" />
-    </div>
+    <main className="min-h-[500px] mt-10 pb-[30px] small:mt-0 xsmall:mt-0 px-24">
+      <div>
+        <Header title="Projects" />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {projects.map((project) => (
+            <div key={project.id}>
+              <Link
+                href={`/projects/${project.title
+                  .toLowerCase()
+                  .replaceAll(" ", "-")}`}
+              >
+                <div className="relative block text-center group">
+                  <AspectRatio
+                    ratio={1 / 1}
+                    className="w-full h-full relative rounded-md"
+                  >
+                    <Image
+                      src={project.projectImage[0].url}
+                      fill
+                      alt={project.title}
+                      className="object-cover rounded-[20px]"
+                    />
+                  </AspectRatio>
+                  <div className="h-full absolute top-0 block w-full overflow-hidden rounded-[20px]">
+                    <div className="h-full translate-y-full text-center w-full group-hover:-translate-y-0 transition-all duration-700 group-hover:bg-blue-50 group-hover:bg-opacity-50 px-10">
+                      <p className="text-[25px]">{project.title}</p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
   );
 };
 
