81bc3c7b4661c6538b287b7bcdc47f352c6689b0
Alex <weiru@hotmail.ca>
2024-04-28 00:03:01 -0400

    Update fetch parameters

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,7 +3,10 @@ import prismadb from "@/lib/prismadb";
 import BelowDisplay from "./components/below-display";
 
 const HomePage = async () => {
-  const images = await prismadb.images.findMany({ where: { width: 6000 } });
+  const images = await prismadb.images.findMany({
+    where: { width: 6000 },
+    orderBy: { id: "asc" },
+  });
 
   return (
     <main className="min-h-[500px] mt-10 pb-[30px] small:mt-0 xsmall:mt-0 px-24">
