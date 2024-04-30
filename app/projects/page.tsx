402793ccafa855226d4a11fdfbd298a3b0e472a5
Alex <weiru@hotmail.ca>
2024-04-29 23:38:55 -0400

    Add description

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,9 +17,10 @@ const ProjectsPage = async () => {
     include: { projectImage: true },
   });
 
+  console.log(projects[0]);
   return (
     <main className="min-h-[500px] mt-10 pb-[30px] small:mt-0 xsmall:mt-0 px-24">
-      <div>
+      <div className="text-center w-full">
         <Header title="Projects" />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {projects.map((project) => (
@@ -41,12 +42,15 @@ const ProjectsPage = async () => {
                       className="object-cover rounded-[20px]"
                     />
                   </AspectRatio>
-                  <div className="h-full absolute top-0 block w-full overflow-hidden rounded-[20px]">
+                  <div className="md:block hidden h-full absolute top-0 w-full overflow-hidden rounded-[20px]">
                     <div className="h-full translate-y-full text-center w-full group-hover:-translate-y-0 transition-all duration-700 group-hover:bg-blue-50 group-hover:bg-opacity-50 px-10">
-                      <p className="text-[25px]">{project.title}</p>
+                      <p className="pt-2 lg:pt-10">
+                        {project.shortDescription}
+                      </p>
                     </div>
                   </div>
                 </div>
+                <div className="text-lg">{project.title}</div>
               </Link>
             </div>
           ))}
