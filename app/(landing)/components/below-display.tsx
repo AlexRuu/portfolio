a73405e0e38356ca6508e2b4d0d0d83aaec9d0c1
Alex <weiru@hotmail.ca>
2024-05-01 21:45:55 -0400

    Fix up page and add latest posts

--- app/(landing)/components/below-display.tsx
+++ app/(landing)/components/below-display.tsx
@@ -1,25 +1,87 @@
 "use client";
 
-import { useWindowDimensions } from "@/hooks/useWindowDimensions";
-import { useEffect, useState } from "react";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Prisma } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
 
-const BelowDisplay = () => {
-  const { height } = useWindowDimensions();
-  const [isMounted, setIsMounted] = useState<boolean>(false);
+type Image = Prisma.ImagesGetPayload<{}>;
+type Project = Prisma.ProjectGetPayload<{ include: { projectImage: true } }>;
+type Recipe = Prisma.RecipeGetPayload<{ include: { RecipeImage: true } }>;
 
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+interface BelowDisplayProps {
+  gallery: Image;
+  project: Project;
+  recipe: Recipe;
+}
 
-  if (!isMounted) {
-    return null;
-  }
+const BelowDisplay: React.FC<BelowDisplayProps> = ({
+  gallery,
+  project,
+  recipe,
+}) => {
+  const categories = [
+    {
+      title: "Gallery",
+      href: "/gallery",
+      image: gallery.url,
+    },
+    {
+      title: "Projects",
+      href: "/projects",
+      image: project.projectImage[0].url,
+    },
+    { title: "Recipes", href: "/recipes", image: recipe.RecipeImage[0].url },
+  ];
 
   return (
-    <div className="block relative">
-      <div className="grid grid-cols-2 w-full">
-        <p className="w-1/2">hi</p>
-        <p className="w-1/2">there</p>
+    <div className="block relative mt-5">
+      <div className="w-full">
+        <div className="w-full text-center justify-center items-center">
+          <h1 className="font-semibold uppercase mb-3 text-2xl">
+            Latest Posts
+          </h1>
+        </div>
+        <div className="flex w-full min-h-[500px] lg:flex-row flex-col">
+          {categories.map((category) => (
+            <div
+              key={category.title}
+              className="lg:w-1/3 w-full lg:mx-5 rounded-[20px] lg:mb-0 mb-10 justify-center text-center"
+            >
+              <Link
+                href={category.href}
+                className="block relative overflow-hidden touch-manipulation rounded-[20px] mb-2"
+              >
+                <div
+                  className="h-auto w-full block"
+                  style={{ backfaceVisibility: "hidden" }}
+                >
+                  <div className="max-w-[1236px] max-h-[1236px] mx-auto">
+                    <div className="pt-[100%] relative rounded-[20px]">
+                      <Image
+                        src={category.image}
+                        fill
+                        alt=""
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        className="absolute top-0 left-0 max-w-full rounded-[20px] hover:scale-110 transition ease-in-out duration-300"
+                        style={{
+                          objectFit: "cover",
+                          objectPosition: "center",
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Link>
+              <Link
+                href={category.href}
+                className="text-xl mt-5 uppercase w-full"
+              >
+                {category.title}
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import HomeDisplay from "@/app/(landing)/components/home-display";
 import prismadb from "@/lib/prismadb";
 import BelowDisplay from "./components/below-display";
@@ -8,11 +10,25 @@ const HomePage = async () => {
     orderBy: { id: "asc" },
   });
 
+  const gallery = await prismadb.images.findFirst({
+    orderBy: { createdAt: "desc" },
+  });
+
+  const project = await prismadb.project.findFirst({
+    include: { projectImage: true },
+    orderBy: { createdAt: "desc" },
+  });
+
+  const recipe = await prismadb.recipe.findFirst({
+    include: { RecipeImage: true },
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
     <main className="min-h-[500px] mt-10 pb-[30px] small:mt-0 xsmall:mt-0 px-20">
       <div className="flex flex-col">
         <HomeDisplay images={images} />
-        <BelowDisplay />
+        <BelowDisplay gallery={gallery} project={project} recipe={recipe} />
       </div>
     </main>
   );
